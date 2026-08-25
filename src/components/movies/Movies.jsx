882c6218f105952d86368ac7db6c4d8cbc399f04
nerina <nerinacoronel00@gmail.com>
2026-08-25 17:41:52 -0300

    Fix Movies

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -5,7 +5,7 @@ import MovieItem from "./MovieItem"
 const Movies = () => {
   return (
     <Container>
-      <Row>
+      <Row className="justify-content-center">
         {moviesList.map((item) => (
           <Col key={item.movieId} className="col-12 col-md-6 col-lg-3 m-2">
             <MovieItem

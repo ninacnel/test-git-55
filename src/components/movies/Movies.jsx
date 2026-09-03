d63d86e2bf22e579d0a5df0457e46bd0d81a474c
nerina <nerinacoronel00@gmail.com>
2026-09-03 14:30:35 -0300

    Removed beers

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { moviesList } from "../data"
 import MovieItem from "./MovieItem"
 import { useState } from "react"

e1724d69f2fe6bc7cf13f9d55b11b3e81739e386
nerina <nerinacoronel00@gmail.com>
2026-09-10 14:07:07 -0300

    Added movieForm

---
+++ src/components/movies/MovieForm.jsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const initialForm = {
+    title: "",
+    director: "",
+    year: 0,
+    duration: 0,
+    cast: [""],
+    country: "",
+    isFree: true,
+    image: "https://i.pinimg.com/1200x/cb/15/97/cb15975ac00a122cbeca0b9fb14307d5.jpg"
+};
+
+const MovieForm = ({ onAddMovie }) => {
+    const [form, setForm] = useState(initialForm);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setForm({
+            ...form,
+            [name]: value
+        });
+    };
+
+    const handleCastChange = (event) => {
+        const { value } = event.target;
+
+        setForm({
+            ...form,
+            cast: value.split(",")
+        });
+    };
+
+    const handleCheck = (event) => {
+        const { name, checked } = event.target;
+
+        setForm({
+            ...form,
+            [name]: checked
+        });
+    };
+
+    const handleAddMovie = (event) => {
+        event.preventDefault();
+
+        const newMovie = {
+            movieId: Math.random(),
+            ...form
+        };
+console.log(newMovie)
+        onAddMovie(newMovie);
+
+        setForm(initialForm);
+    };
+
+    return (
+        <Form onSubmit={handleAddMovie} className="w-50">
+
+            <Form.Group className="mb-3">
+                <Form.Label>Título</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="title"
+                    value={form.title}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Director</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="director"
+                    value={form.director}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Año</Form.Label>
+                <Form.Control
+                    type="number"
+                    name="year"
+                    value={form.year}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Duración</Form.Label>
+                <Form.Control
+                    type="number"
+                    name="duration"
+                    value={form.duration}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>País</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="country"
+                    value={form.country}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Actores</Form.Label>
+
+                <Form.Control
+                    type="text"
+                    value={form.cast}
+                    onChange={handleCastChange}
+                    placeholder="Separar nombres por comas"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Check
+                    type="checkbox"
+                    label="Película gratuita"
+                    name="isFree"
+                    checked={form.isFree}
+                    onChange={handleCheck}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Imagen</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="image"
+                    value={form.image}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Button type="submit">
+                Agregar
+            </Button>
+
+        </Form>
+    );
+};
+
+export default MovieForm;
--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -2,11 +2,13 @@ import { Col, Container, Row } from "react-bootstrap"
 import { moviesList } from "../data"
 import MovieItem from "./MovieItem"
 import { useState } from "react"
+import MovieForm from "./MovieForm"
 
 const Movies = () => {
   // useState
   const [totalViews, setTotalViews] = useState(0);
   const [lastMovie, setLastMovie] = useState("");
+  const [movies, setMovies] = useState(moviesList);
 
   const handleChangeTotalViews = () => {
     setTotalViews((prevState) => prevState + 1);
@@ -16,14 +18,20 @@ const Movies = () => {
     setLastMovie(title);
   }
 
+  const handleAddMovie = (newMovie) => {
+    setMovies((prevState) => [ newMovie, ...prevState]);
+  }
+
   return (
     <>
       <h1>Reproducciones totales: {totalViews}</h1>
       <h2>Ultima pelicula vista: {lastMovie}</h2>
 
+      <MovieForm onAddMovie={handleAddMovie}/>
+
       <Container>
         <Row className="justify-content-center">
-          {moviesList.map((item) => (
+          {movies.map((item) => (
             <Col key={item.movieId} className="col-12 col-md-6 col-lg-3 m-2">
               <MovieItem
                 movie={item}
